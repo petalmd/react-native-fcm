d2032c01722821bd5a3ee4d112a5bff338b71901
Luisa Verza <lverza@petalmd.com>
2019-12-30 10:48:26 -0500

    Revert "update to androidx"
    This reverts commit f696694ee9ed8b410a327f815e68e6fa58596c72.

--- android/src/main/java/com/evollu/react/fcm/InstanceIdService.java
+++ android/src/main/java/com/evollu/react/fcm/InstanceIdService.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
